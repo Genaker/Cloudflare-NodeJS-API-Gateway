4578392d831bca5bda7077f2eb6984e3bd6c0f46
Yegor Shytikov <egorshitikov@gmail.com>
2025-01-31 11:36:17 -0800

    Update API-SNIPPET.js

--- API-SNIPPET.js
+++ API-SNIPPET.js
@@ -1,13 +1,36 @@
-  /**
-  * SET Snippet Rule to: 
-  * (http.request.full_uri wildcard "*sub.domain.com/rest/*")
-  */
+/**
+ * SET Snippet Rule to: 
+ * (http.request.full_uri wildcard "*sub.domain.com/rest/*")
+ */
+
+// Global Vars is user-defined. Modify them for your needs
+
+const apiSecretEnabled = true;
+const whitelistEnabled = true;
+
+// Define a whitelist of allowed URL patterns
+const whitelist = [
+  /\/rest\/?$/,
+  /\/rest\/endpoint1$/,
+  /\/rest\/endpoint2$/,
+  /\/rest\/.*/
+];
+
 export default {
   async fetch(request) {
 
     const apiSecret = request.headers.get("CF-API-Secret");
+    const URL = request.url;
+    const isWhitelisted = whitelist.some(pattern => pattern.test(URL));
 
-    if (apiSecret !== "12345") {
+    if (whitelistEnabled && !isWhitelisted) {
+      return new Response(JSON.stringify({ message: "Access Denied: URL not allowed" }), {
+        status: 403,
+        headers: { "Content-Type": "application/json" }
+      });
+    }
+
+    if (apiSecretEnabled && apiSecret !== "12345") {
       return new Response(JSON.stringify({ message: "API Access Denied: Missing CF-API-Secret" }), {
         status: 401,
         headers: { "Content-Type": "application/json" }
